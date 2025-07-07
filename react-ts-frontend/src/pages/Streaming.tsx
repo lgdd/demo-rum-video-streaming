7fce41acb34a2fb4d65acc336c1d02236b6cac4a
lgdd <3501902+lgdd@users.noreply.github.com>
2025-07-07 11:41:14 +0200

    fix: back to videos color button

--- react-ts-frontend/src/pages/Streaming.tsx
+++ react-ts-frontend/src/pages/Streaming.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import MuxPlayer from "@mux/mux-player-react";
-import { Heading } from "@chakra-ui/react";
+import { Flex, Heading, IconButton } from "@chakra-ui/react";
+import { LuArrowLeft } from "react-icons/lu";
 
 const Streaming = () => {
   const params = useParams();
@@ -10,7 +11,14 @@ const Streaming = () => {
 
   return (
     <>
-      <Heading marginBottom={2}>{params.title}</Heading>
+      <Flex direction="flex-start" alignItems="center" gap={2}>
+        <Link to="/home">
+          <IconButton bg="transparent" color={{ base: "black", _dark: "white" }}>
+            <LuArrowLeft />
+          </IconButton>
+        </Link>
+        <Heading my={2}>{params.title}</Heading>
+      </Flex>
       <MuxPlayer
         playbackId={params.playbackId}
         metadata={{
